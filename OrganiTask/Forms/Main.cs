0b5022d927b02cddb7099971fe52532ce45c696c
Javithor360 <floresmejia004@gmail.com>
2025-03-10 22:55:54 -0600

    fix: regenerated database entities & fixed exiting implementations

--- OrganiTask/Forms/Main.cs
+++ OrganiTask/Forms/Main.cs
@@ -38,12 +38,12 @@ namespace OrganiTask.Forms
             using (var context = new OrganiTaskDB())
             {
                 // de manera temporal, obtenemos todos los usuarios
-                var users = context.Users.Include(u => u.Boards).Select(u => new
+                var users = context.Users.Include(u => u.Board).Select(u => new
                 {
                     u.Id,
                     u.Username,
                     u.Email,
-                    NumeroTableros = u.Boards.Count()
+                    NumeroTableros = u.Board.Count()
                 }).ToList();
 
                 // y los cargamos en el DataGridView con la información recopilada
