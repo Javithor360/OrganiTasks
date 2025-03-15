aae651ddb773fff1fdbae7e8397b8418737dc69d
Javithor360 <floresmejia004@gmail.com>
2025-03-15 17:00:09 -0600

    feat: implemented Kanban layout & connected some thing to the database

--- OrganiTask/Forms/Main.cs
+++ OrganiTask/Forms/Main.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 
 using OrganiTask.Entities;
 using OrganiTask.Util;
+using OrganiTask.Forms.Test;
 
 /*
  * CONSIDERACIONES IMPORTANTES PARA TODO EL PROYECTO:
@@ -54,6 +55,18 @@ namespace OrganiTask.Forms
         private void Main_Load(object sender, EventArgs e)
         {
             Refresh();
+        }
+
+        private void btnMainTest_Click(object sender, EventArgs e)
+        {
+            KanbanDashboard kanban = new KanbanDashboard(2, 4);
+            kanban.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CategoriesManagement categoriesManagement = new CategoriesManagement();
+            categoriesManagement.Show();
         }
     }
 }
