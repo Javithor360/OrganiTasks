d2e4351396cf8f0564d4837b953231d97d43c968
JustinDMXD <168238973+JustinDMXD@users.noreply.github.com>
2025-03-14 18:24:42 -0700

    Classes

--- OrganiTask/Util/LinkedList.cs
+++ OrganiTask/Util/LinkedList.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace OrganiTask.Util
 {
-    internal class LinkedList
+    public class LinkedList<T>
     {
+        private class Nodo
+        {
+            public T Data;
+            public Nodo Next;
+
+            public Nodo(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+        private Nodo head;
+
+        public void Add(T data)
+        {
+            Nodo newNode = new Nodo(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Nodo current = head;
+                while(current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Remove(T data)
+        {
+            if (head == null) return;
+
+            if (head.Data.Equals(data))
+            {
+                {
+                    head = head.Next;
+                    return;
+                }
+
+                Nodo current = head;
+
+                while (current.Next != null && !current.Next.Data.Equals(data))
+                {
+                    current = current.Next;
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next = current.Next.Next;
+                }
+            }
+        }
+
     }
 }
