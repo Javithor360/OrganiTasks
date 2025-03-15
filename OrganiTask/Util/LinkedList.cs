83647a307b7307cc3c832d69917369dfa601cf46
JustinDMXD <168238973+JustinDMXD@users.noreply.github.com>
2025-03-14 18:23:00 -0700

    Create LinkedList.cs

---
+++ OrganiTask/Util/LinkedList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganiTask.Util
+{
+    internal class LinkedList
+    {
+    }
+}
