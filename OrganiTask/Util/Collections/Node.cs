40ae6bf4decf1c7941dd58d863343b415db5b2cf
Javithor360 <floresmejia004@gmail.com>
2025-03-23 00:21:02 -0600

    feat: implemented some extra getters & methods to OrganiList class with extra refactors & documentation

--- OrganiTask/Util/Collections/Node.cs
+++ OrganiTask/Util/Collections/Node.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 namespace OrganiTask.Util.Collections
 {
     /// <summary>
-    /// Clase que maneja los nodos de la lista
+    /// Clase nodo para la lista doblemente enlazada que contiene referencias al siguiente y anterior nodo
     /// </summary>
-    /// <typeparam name="T">Tipo de dato que contendrá el nodo</typeparam>
+    /// <typeparam name="T">Tipo de elementos en el nodo</typeparam>
     public class Node<T>
     {
-        public T Value { get; set; } // Tipo de dato genérico y valor que contendrá el nodo
-        public Node<T> Next { get; set; } // Instancia del siguiente nodo
-        public Node<T> Previous { get; set; } // Instancia del nodo anterior
+        public T Value { get; set; } // Valor que almacena el nodo
+        public Node<T> Next { get; set; } // Referencia al siguiente nodo en la lista
+        public Node<T> Previous { get; set; } // Referencia al nodo anterior en la lista
 
         /// <summary>
-        /// Constructor default de la clase
+        /// Constructor que crea un nodo con un valor específico
         /// </summary>
         /// <param name="value">Valor que almacenará la instancia actual del nodo</param>
         public Node(T value)
--- OrganiTask/Util/Collections/OrganiList.cs
+++ OrganiTask/Util/Collections/OrganiList.cs
@@ -9,9 +9,7 @@ using System.Threading.Tasks;
 namespace OrganiTask.Util.Collections
 {
     /// <summary>
-    /// Estructura de dato dinámica lineal que permite almacenar elementos de un tipo genérico
-    /// y que implementa la interfaz IEnumerable para poder recorrer sus elementos
-    /// además de implementar los métodos necesarios para manipular la lista
+    /// Representación de una lista doblemente enlazada como estructura de dato dinámica lineal
     /// </summary>
     /// <typeparam name="T">Tipo de dato que tendrán los elementos de la lista</typeparam>
     public class OrganiList<T> : IEnumerable<T> where T : IComparable<T>
@@ -19,9 +17,6 @@ namespace OrganiTask.Util.Collections
         private Node<T> head; // Nodo inicial de la lista
         private Node<T> tail; // Nodo final de la lista
         private int count; // Cantidad de elementos en la lista
-
-        // Getter de la cantidad de elementos en la lista
-        public int Count => count;
 
         /// <summary>
         /// Constructor default de la clase que inicializa sus elementos vacíos
@@ -37,33 +32,35 @@ namespace OrganiTask.Util.Collections
         /// Agrega un elemento al inicio de la lista
         /// </summary>
         /// <param name="value">Valor que se desea agregar a la lista</param>
-        public void AddFirst(T value)
+        /// <returns>Nodo que se agregó a la lista</returns>
+        public Node<T> AddFirst(T value)
         {
             // Se crea un nuevo nodo con el valor deseado
             Node<T> newNode = new Node<T>(value);
+            if (IsEmpty)
+                head = tail = newNode; // Si la lista está vacía, el nuevo nodo es el único nodo
+            else
             {
-                if (head == null)
-                    head = tail = newNode; // Si la lista está vacía, el nuevo nodo es el único nodo
-                else
-                {
-                    // Si la lista no está vacía, se agrega el nuevo nodo al inicio de la lista
-                    newNode.Next = head; // Temporalmente, el siguiente nodo del nuevo nodo es el nodo inicial
-                    head.Previous = newNode; // El nodo anterior del nodo inicial es el nuevo nodo
-                    head = newNode; // El nuevo nodo es el nodo inicial
-                }
-                count++; // Se incrementa la cantidad de elementos en la lista
-            }   
+                // Si la lista no está vacía, se agrega el nuevo nodo al inicio de la lista
+                newNode.Next = head; // Temporalmente, el siguiente nodo del nuevo nodo es el nodo inicial
+                head.Previous = newNode; // El nodo anterior del nodo inicial es el nuevo nodo
+                head = newNode; // El nuevo nodo es el nodo inicial
+            }
+
+            count++; // Se incrementa la cantidad de elementos en la lista
+            return newNode; // Se devuelve el nodo insertado
         }
 
         /// <summary>
         /// Agrega un elemento al final de la lista
         /// </summary>
         /// <param name="value">Valor que se desea agregar a la lista</param>
-        public void AddLast(T value)
+        /// <returns>Nodo que se agregó a la lista</returns>
+        public Node<T> AddLast(T value)
         {
             // Se crea un nuevo nodo con el valor deseado
             Node<T> newNode = new Node<T>(value);
-            if (head == null)
+            if (IsEmpty)
                 head = tail = newNode; // Si la lista está vacía, el nuevo nodo es el único nodo
             else
             {
@@ -72,56 +69,153 @@ namespace OrganiTask.Util.Collections
                 newNode.Previous = tail; // El nodo anterior del nuevo nodo es el nodo final
                 tail = newNode; // El nuevo nodo es el nodo final
             }
+
             count++; // Se incrementa la cantidad de elementos en la lista
+            return newNode; // Se devuelve el nodo insertado
         }
 
         /// <summary>
-        /// Agrega un elemento en una posición específica de la lista 
+        /// Inserta un elemento en una posición específica de la lista
         /// </summary>
-        /// <param name="values"></param>
+        /// <param name="position">Posición en la que se desea insertar el elemento</param>
+        /// <param name="value">Valor que se desea insertar en la lista</param>
+        /// <returns>Nodo que se insertó en la lista</returns>
+        public Node<T> Insert(int position, T value)
+        {
+            // Si la posición es inválida, no se hace nada
+            if (position < 0 || position > count) 
+                return null;
+
+            // Si la posición es 0, se agrega al inicio de la lista
+            if (position == 0) 
+                return AddFirst(value); 
+
+            // Si la posición es igual a la cantidad de elementos, se agrega al final de la lista
+            if (position == count)
+                return AddLast(value);
+
+            // Nodo temporal que recorrerá la lista empezando por el nodo inicial
+            Node<T> current = head;
+
+            // Se recorre la lista hasta llegar a la posición deseada
+            for (int i = 0; i < position; i++)
+                current = current.Next; // Se avanza al siguiente nodo
+
+            // Se crea un nuevo nodo con el valor deseado
+            Node<T> newNode = new Node<T>(value);
+            newNode.Next = current; // El siguiente nodo del nuevo nodo es el nodo actual
+            newNode.Previous = current.Previous; // El nodo anterior del nuevo nodo es el nodo anterior del nodo actual
+            current.Previous.Next = newNode; // El siguiente nodo del nodo anterior del nodo actual es el nuevo nodo
+            current.Previous = newNode; // El nodo anterior del nodo actual es el nuevo nodo
+            count++; // Se incrementa la cantidad de elementos en la lista
+
+            return newNode; // Se devuelve el nodo insertado
+        }
+
+        /// <summary>
+        /// Agrega múltiples elementos al final de la lista
+        /// </summary>
+        /// <param name="values">Valores que se desean agregar a la lista</param>
         public void BulkAdd(IEnumerable<T> values)
         {
             // Se recorren los elementos y se agregan a la lista
             foreach (var value in values)
             {
-                AddLast(value);
+                AddLast(value); // Se agrega el valor al final de la lista
             }
         }
 
         /// <summary>
-        /// Busca un elemento en la lista y devuelve el nodo que lo contiene
+        /// Elimina el primer elemento de la lista
         /// </summary>
-        /// <param name="value">Valor que se desea buscar en la lista</param>
-        /// <returns></returns>
-        public Node<T> Find(T value)
+        /// <returns>Nodo que se eliminó de la lista</returns>
+        public Node<T> RemoveFirst()
         {
-            // Se crea un nodo temporal que recorrerá la lista
-            Node<T> current = head;
-            // Mientras el nodo actual no sea nulo se recorre la lista
-            while (current != null)
-            {
-                // Si el valor del nodo actual es igual al valor buscado, se devuelve el nodo actual
-                if (current.Value.Equals(value))
-                    return current;
-                current = current.Next; // Se avanza al siguiente nodo
-            }
-            return null; // Si no se encuentra el valor, se devuelve nulo
+            // Si la lista está vacía, no se hace nada
+            if (IsEmpty) 
+                return null;
+
+            Node<T> node = head; // Nodo que se eliminará de la lista
+            head = head.Next; // El nodo inicial es el siguiente nodo
+
+            // Si el nodo inicial no es nulo, el nodo anterior del nodo inicial es nulo
+            if (head != null) 
+                head.Previous = null;
+
+            count--; // Se decrementa la cantidad de elementos en la lista
+            return node; // Se devuelve el nodo eliminado
+        }
+
+        /// <summary>
+        /// Elimina el último elemento de la lista
+        /// </summary>
+        /// <returns>Nodo que se eliminó de la lista</returns>
+        public Node<T> RemoveLast()
+        {
+            // Si la lista está vacía, no se hace nada
+            if (IsEmpty)
+                return null;
+            Node<T> node = tail; // Nodo que se eliminará de la lista
+            tail = tail.Previous; // El nodo final es el nodo anterior
+            // Si el nodo final no es nulo, el siguiente nodo del nodo final es nulo
+            if (tail != null)
+                tail.Next = null;
+
+            count--; // Se decrementa la cantidad de elementos en la lista
+            return node; // Se devuelve el nodo eliminado
         }
 
         /// <summary>
         /// Elimina un elemento de la lista
         /// </summary>
         /// <param name="node">Nodo que se desea eliminar de la lista</param>
-        public void Remove(Node<T> node)
+        public Node<T> Remove(Node<T> node)
         {
             // Si el nodo es nulo, no se hace nada
-            if (node == null) return;
+            if (node == null) return null;
 
-            if (node == head) head = node.Next; // Si el nodo es el inicial, el siguiente nodo es el nuevo inicial
-            if (node == tail) tail = node.Previous; // Si el nodo es el final, el nodo anterior es el nuevo final
-            if (node.Previous != null) node.Previous.Next = node.Next; // Si el nodo anterior no es nulo, el siguiente nodo del nodo anterior es el siguiente nodo del nodo actual
-            if (node.Next != null) node.Next.Previous = node.Previous; // Si el siguiente nodo no es nulo, el nodo anterior del siguiente nodo es el nodo anterior del nodo actual
+            // Si el nodo es el inicial, el siguiente nodo es el nuevo inicial
+            if (node == head) 
+                head = node.Next;
+
+            // Si el nodo es el final, el nodo anterior es el nuevo final
+            if (node == tail) 
+                tail = node.Previous;
+
+            // Si el nodo anterior no es nulo, el siguiente nodo del nodo anterior es el siguiente nodo del nodo actual
+            if (node.Previous != null) 
+                node.Previous.Next = node.Next;
+
+            // Si el siguiente nodo no es nulo, el nodo anterior del siguiente nodo es el nodo anterior del nodo actual
+            if (node.Next != null) 
+                node.Next.Previous = node.Previous;
+
             count--; // Se decrementa la cantidad de elementos en la lista
+            return node; // Se devuelve el nodo eliminado
+        }
+
+        /// <summary>
+        /// Elimina un elemento de la lista en una posición específica
+        /// </summary>
+        /// <param name="position">Posición del elemento que se desea eliminar</param>
+        /// <returns>Nodo que se eliminó de la lista</returns>
+        public Node<T> RemoveAt(int position)
+        {
+            // Si la posición es inválida, no se hace nada
+            if (position < 0 || position >= count) 
+                return null;
+
+            // Nodo temporal que recorrerá la lista empezando por el nodo inicial
+            Node<T> current = head;
+
+            // Se recorre la lista hasta llegar a la posición deseada
+            for (int i = 0; i < position; i++)
+            {
+                current = current.Next; // Se avanza al siguiente nodo
+            }
+
+            // Se elimina el nodo actual de la lista y se devuelve inmediatamente
+            return Remove(current);
         }
 
         /// <summary>
@@ -166,38 +260,6 @@ namespace OrganiTask.Util.Collections
         }
 
         /// <summary>
-        /// Inserta un elemento en una posición específica de la lista
-        /// </summary>
-        /// <param name="position">Posición en la que se desea insertar el elemento</param>
-        /// <param name="value">Valor que se desea insertar en la lista</param>
-        public void Insert(int position, T value)
-        {
-            // Si la posición es inválida, no se hace nada
-            if (position < 0 || position > count) return;
-
-            // Si la posición es 0, se agrega al inicio de la lista
-            if (position == 0) { AddFirst(value); return; }
-
-            // Si la posición es igual a la cantidad de elementos, se agrega al final de la lista
-            if (position == count) { AddLast(value); return; }
-
-            // Nodo temporal que recorrerá la lista empezando por el nodo inicial
-            Node<T> current = head;
-
-            // Se recorre la lista hasta llegar a la posición deseada
-            for (int i = 0; i < position; i++)
-                current = current.Next; // Se avanza al siguiente nodo
-
-            // Se crea un nuevo nodo con el valor deseado
-            Node<T> newNode = new Node<T>(value);
-            newNode.Next = current; // El siguiente nodo del nuevo nodo es el nodo actual
-            newNode.Previous = current.Previous; // El nodo anterior del nuevo nodo es el nodo anterior del nodo actual
-            current.Previous.Next = newNode; // El siguiente nodo del nodo anterior del nodo actual es el nuevo nodo
-            current.Previous = newNode; // El nodo anterior del nodo actual es el nuevo nodo
-            count++; // Se incrementa la cantidad de elementos en la lista
-        }
-
-        /// <summary>
         /// Ordena los elementos de la lista
         /// NOTA: Este método solo funciona con tipos de datos que implementen la interfaz IComparable
         /// </summary>
@@ -232,12 +294,83 @@ namespace OrganiTask.Util.Collections
         }
 
         /// <summary>
+        /// Busca un elemento en la lista y devuelve el nodo que lo contiene
+        /// </summary>
+        /// <param name="value">Valor que se desea buscar en la lista</param>
+        /// <returns>Nodo que contiene el valor buscado</returns>
+        public Node<T> Find(T value)
+        {
+            // Se crea un nodo temporal que recorrerá la lista
+            Node<T> current = head;
+            // Mientras el nodo actual no sea nulo se recorre la lista
+            while (current != null)
+            {
+                // Si el valor del nodo actual es igual al valor buscado, se devuelve el nodo actual
+                if (current.Value.Equals(value))
+                    return current;
+                current = current.Next; // Se avanza al siguiente nodo
+            }
+            return null; // Si no se encuentra el valor, se devuelve nulo
+        }
+
+        /// <summary>
+        /// Busca un elemento en la lista por posición y devuelve el nodo que lo contiene
+        /// </summary>
+        /// <param name="position">Posición del elemento que se desea buscar</param>
+        /// <returns>Nodo que contiene el valor buscado</returns>
+        public Node<T> FindAt(int position)
+        {
+            // Si la posición es inválida, no se hace nada
+            if (position < 0 || position >= count)
+                return null;
+
+            // Nodo temporal que recorrerá la lista empezando por el nodo inicial
+            Node<T> current = head;
+
+            // Se recorre la lista hasta llegar a la posición deseada
+            for (int i = 0; i < position; i++)
+                current = current.Next; // Se avanza al siguiente nodo
+
+            return current; // Se devuelve el nodo encontrado
+        }
+
+        /// <summary>
+        /// Verifica si un elemento se encuentra en la lista
+        /// </summary>
+        /// <param name="value">Valor que se desea buscar en la lista</param>
+        /// <returns>Verdadero si el valor se encuentra en la lista, falso en caso contrario</returns>
+        public bool Contains(T value)
+        {
+            return Find(value) != null;
+        }
+
+        /// <summary>
         /// Elimina todos los elementos de la lista
         /// </summary>
         public void Clear()
         {
             head = tail = null;
             count = 0;
+        }
+
+        /// <summary>
+        /// Convierte la lista en un arreglo
+        /// </summary>
+        /// <returns>Arreglo con los elementos de la lista</returns>
+        public T[] ToArray()
+        {
+            T[] array = new T[count]; // Se crea un arreglo con la cantidad de elementos en la lista
+            Node<T> current = head; // Nodo temporal que recorrerá la lista empezando por el nodo inicial
+            int i = 0; // Índice del arreglo
+
+            // Mientras el nodo actual no sea nulo se recorre la lista
+            while (current != null)
+            {
+                array[i++] = current.Value; // Se agrega el valor del nodo actual al arreglo
+                current = current.Next; // Se avanza al siguiente nodo
+            }
+
+            return array; // Se devuelve el arreglo con los elementos de la lista
         }
 
         /// <summary>
@@ -275,6 +408,17 @@ namespace OrganiTask.Util.Collections
         // Getter de la interfaz IEnumerable para poder recorrer los elementos de la lista y que sea compatible con foreach
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        // Getter del primer elemento de la lista
+        public T First => head.Value;
+
+        // Getter del último elemento de la lista
+        public T Last => tail.Value;
+
+        // Getter de la cantidad de elementos en la lista
+        public int Count => count;
+
+        // Getter que indica si la lista está vacía
+        public bool IsEmpty => count == 0;
     }
 }
 
